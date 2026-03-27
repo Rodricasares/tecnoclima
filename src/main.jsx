b2ae05a88f6c450bdb3767c211addabaa5fef64f
Rodrigo Cantudo <rodrigocantudo@gmail.com>
2026-03-27 12:13:21 +0100

    analytic vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from "./App";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-
+import { Analytics } from "@vercel/analytics/react"
 import "./i18n/i18n";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
